939a4914e243ded2c5a6e13b2013e5b5b23ca159
Kateryna Gorokhova <kateryna.gorokhova@netlight.com>
2017-07-24 00:10:12 +0200

    glitchy animation

---
+++ assets/scripts/animate.js
@@ -0,0 +1,33 @@
+var toppos = 0;
+var direction = "down";
+
+function startAnimation() {
+	setInterval(animateBird, 50) // run code every 50 milliseconds
+}
+
+function animateBird() {
+	if (toppos > 300 && direction == "down") {
+		direction = "up";
+	}
+	if (toppos < 5 && direction == "up") {
+		direction = "down";
+	}
+	return direction == "up" ? animateUp() : animateDown();
+}
+
+function animateUp() {
+	console.log("animateUp");
+	var bird = document.getElementById('bird');
+	toppos -= 5;
+	bird.style.top = toppos + 'px';
+}
+
+function animateDown() {
+		console.log("animateDown");
+
+	var bird = document.getElementById('bird');
+	toppos += 5;
+	bird.style.top = toppos + 'px';
+}
+
+document.addEventListener("DOMContentLoaded", startAnimation);
